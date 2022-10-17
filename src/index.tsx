7ff25712918db37659c5e45be95692e649b7ff1d
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-10-17 21:26:44 +0300

    project path have been fixed

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
